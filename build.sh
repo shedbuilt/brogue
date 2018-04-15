19f1ceff7a2afefd17d9234a1f1bd7d8f55eb9ec
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:14:11 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PATCHDIR}/non-x86-compilation.patch"
-make -j $SHED_NUMJOBS curses || exit 1
-install -Dm755 bin/brogue "${SHED_FAKEROOT}/usr/bin/brogue"
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/non-x86-compilation.patch"
+make -j $SHED_NUM_JOBS curses || exit 1
+install -Dm755 bin/brogue "${SHED_FAKE_ROOT}/usr/bin/brogue"
 
 
